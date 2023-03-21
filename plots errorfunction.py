8936a5b1501212ba3a4ff7453779afe4dcd10b07
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-03-20 22:36:44 -0500

    Update plots errorfunction.py

--- plots errorfunction.py
+++ plots errorfunction.py
@@ -21,12 +21,15 @@ def train(n):
     for i in range(0,n): #run(trins from) entire datasaet for each epoch
         for j in range(0,len(points)): #run for each datapoint
             y=a*points[j][0]+b        
+            error=points[j][1]-y #points[j][1] is predicted y value. y is the actual from the dataset.
+            a=a+points[j][0]*error*learningRate #update a (slope). Points[j][0] is the x value from the dataset.
             b=b+error*learningRate #update b
             errorSum += error**2
         errorSums.append(errorSum) #makes a list of the errorSum of each EPOCH 
         numbers.append(i) # list of numbers from 1 to epoch. x-values in error-plot
         errorSum=0 # reset sum of errors after each epoch
     drawErrors(numbers,errorSums)
+    return(a,b)
 
 fig = plt.figure() #defines plot area
 train(epochs)
